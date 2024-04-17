80efad6656cfd38f3e2a6f0369f4dd54af5a8e74
mk0029 <mkhicher404@gmail.com>
2024-04-17 11:56:47 +0530

    add new effact-v2 in header

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,17 +1,53 @@
-        let prevScrollPosition = 0;
+        // let prevScrollPosition = 0;
+        // let isHeaderVisible = false;
+        // function handleScroll() {
+        //     const currentScrollPosition = window.scrollY;
+        //     const visibleHeader = currentScrollPosition > 1 ? prevScrollPosition > currentScrollPosition : false;
+        //     prevScrollPosition = currentScrollPosition;
+        //     isHeaderVisible = visibleHeader;
+        //     const contentContainer = document.querySelector(".content-container");
+        //     const navBtn = document.querySelector(".get_nav_btn");
+        //     if (isHeaderVisible) {
+        //         contentContainer.classList.add("background_blur_header", "top_0");
+        //         navBtn.classList.remove("d-none");
+        //     } else {
+        //         contentContainer.classList.remove("top_0");
+        //     }
+        // }
+// window.addEventListener("scroll", handleScroll);
+        
+     let prevScrollPosition = 0;
         let isHeaderVisible = false;
+
         function handleScroll() {
             const currentScrollPosition = window.scrollY;
-            const visibleHeader = currentScrollPosition > 1 ? prevScrollPosition > currentScrollPosition : false;
+            let visibleHeader = isHeaderVisible;
+
+            const contentContainer = document.querySelector(".content-container");
+            const getNavBtn = document.querySelector(".get_nav_btn");
+
+            if (currentScrollPosition > 100) {
+                contentContainer.classList.add("hide_header");
+
+                if (currentScrollPosition > 768) {
+                    visibleHeader = prevScrollPosition > currentScrollPosition;
+                    contentContainer.classList.add("background_blur_header");
+                    contentContainer.classList.remove("hide_header");
+                    getNavBtn.classList.remove("d-none");
+                }
+            } else {
+                visibleHeader = false;
+                contentContainer.classList.remove("hide_header");
+                if (contentContainer.classList.contains("background_blur_header")) {
+                    contentContainer.classList.remove("background_blur_header");
+                    getNavBtn.classList.add("d-none");
+                }
+            }
             prevScrollPosition = currentScrollPosition;
             isHeaderVisible = visibleHeader;
-            const contentContainer = document.querySelector(".content-container");
-            const navBtn = document.querySelector(".get_nav_btn");
-            if (isHeaderVisible) {
-                contentContainer.classList.add("background_blur_header", "top_0");
-                navBtn.classList.remove("d-none");
-            } else {
-                contentContainer.classList.remove("top_0");
+            if (currentScrollPosition > 1000 && !isHeaderVisible && contentContainer.classList.contains("background_blur_header")) {
+                contentContainer.classList.add("hide_header");
             }
         }
+
         window.addEventListener("scroll", handleScroll);
