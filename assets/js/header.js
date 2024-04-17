3c700b968d1292186df26c383295cc41f17b2f1b
mk0029 <mkhicher404@gmail.com>
2024-04-17 20:19:51 +0530

    work on points

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,21 +1,4 @@
-        // let prevScrollPosition = 0;
-        // let isHeaderVisible = false;
-        // function handleScroll() {
-        //     const currentScrollPosition = window.scrollY;
-        //     const visibleHeader = currentScrollPosition > 1 ? prevScrollPosition > currentScrollPosition : false;
-        //     prevScrollPosition = currentScrollPosition;
-        //     isHeaderVisible = visibleHeader;
-        //     const contentContainer = document.querySelector(".content-container");
-        //     const navBtn = document.querySelector(".get_nav_btn");
-        //     if (isHeaderVisible) {
-        //         contentContainer.classList.add("background_blur_header", "top_0");
-        //         navBtn.classList.remove("d-none");
-        //     } else {
-        //         contentContainer.classList.remove("top_0");
-        //     }
-        // }
-// window.addEventListener("scroll", handleScroll);
-        
+
      let prevScrollPosition = 0;
         let isHeaderVisible = false;
 
@@ -45,9 +28,7 @@
             }
             prevScrollPosition = currentScrollPosition;
             isHeaderVisible = visibleHeader;
-            if (currentScrollPosition >  window.innerHeight+300 && !isHeaderVisible && contentContainer.classList.contains("background_blur_header")) {
-                contentContainer.classList.add("hide_header");
-            }
+      
         }
 
         window.addEventListener("scroll", handleScroll);
