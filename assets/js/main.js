70df6ba13b797d2316fe070f94dd34996f46b965
anjali-chanda <anjalichanda2019@gmail.com>
2024-04-18 21:51:48 +0530

    update two points

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,56 +18,61 @@ $(document).ready(function () {
 });
 
 //   <!-- YEAR FUNCTION -->
-var currentYear = new Date().getFullYear();
-document.getElementById("currentYear").innerText = currentYear;
+// var currentYear = new Date().getFullYear();
+// document.getElementById("currentYear").innerText = currentYear;
+
+var currentYearElements = document.getElementsByClassName("currentYear");
+for (var i = 0; i < currentYearElements.length; i++) {
+    currentYearElements[i].innerText = new Date().getFullYear();
+}
 
 // JOIN COMMUNITY SLIDER CODE
- $(".slider_new_custom").slick({
-     dots: true,
-     infinite: true,
-     speed: 0,
-     slidesToShow: 5,
-     centerMode: true,
-     focusOnSelect: true,
-     centerPadding: 60,
-     slidesToScroll: 1,
-     draggable: false,
-     accessibility: false,
-     variableWidth: true,
-     prevArrow: $(".custom-prev"),
-     nextArrow: $(".custom-next"),
-     responsive: [{
-             breakpoint: 2000,
-             settings: {
-                 slidesToShow: 5,
+$(".slider_new_custom").slick({
+    dots: true,
+    infinite: true,
+    speed: 0,
+    slidesToShow: 5,
+    centerMode: true,
+    focusOnSelect: true,
+    centerPadding: 60,
+    slidesToScroll: 1,
+    draggable: false,
+    accessibility: false,
+    variableWidth: true,
+    prevArrow: $(".custom-prev"),
+    nextArrow: $(".custom-next"),
+    responsive: [{
+            breakpoint: 2000,
+            settings: {
+                slidesToShow: 5,
 
-             },
-         },
-         {
-             breakpoint: 992,
-             settings: {
-                 slidesToShow: 3,
-                 slidesToScroll: 1,
-                 centerMode: false,
-             },
-         },
-         {
-             breakpoint: 767.9,
-             settings: {
-                 slidesToShow: 2,
-                 slidesToScroll: 1,
-                 centerMode: false,
-                 centerPadding: 30,
-                 speed: 300,
-             },
-         },
-         {
-             breakpoint: 425,
-             settings: {
-                 slidesToShow: 1,
-                 slidesToScroll: 2,
-                 speed: 300,
-             },
-         },
-     ],
- });
+            },
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                centerMode: false,
+            },
+        },
+        {
+            breakpoint: 767.9,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                centerMode: false,
+                centerPadding: 30,
+                speed: 300,
+            },
+        },
+        {
+            breakpoint: 425,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 2,
+                speed: 300,
+            },
+        },
+    ],
+});
