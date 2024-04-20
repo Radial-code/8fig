422769f6c3c455f1de9cfe0bfb954ddb91a282b0
mk0029 <mkhicher404@gmail.com>
2024-04-20 11:16:10 +0530

    update  css

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,62 +17,82 @@ $(document).ready(function () {
     });
 });
 
-//   <!-- YEAR FUNCTION -->
-// var currentYear = new Date().getFullYear();
-// document.getElementById("currentYear").innerText = currentYear;
-
 var currentYearElements = document.getElementsByClassName("currentYear");
 for (var i = 0; i < currentYearElements.length; i++) {
     currentYearElements[i].innerText = new Date().getFullYear();
 }
+ var video = document.getElementById("myVideo");
+    var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+    if (isSafari) {
+      video.setAttribute("playsinline", "");
+      video.setAttribute("webkit-playsinline", "");
+    }
 
+    function backToTopOnReload() {
+      window.onload = function () {
+        // Scroll the window to the top
+        window.scrollTo(0, 0);
+      }
+    }
+
+    // Call the function to enable back to top on window reload
+    backToTopOnReload();
 // JOIN COMMUNITY SLIDER CODE
-$(".slider_new_custom").slick({
-    dots: true,
-    infinite: true,
-    speed: 0,
-    slidesToShow: 5,
-    centerMode: true,
-    focusOnSelect: true,
-    centerPadding: 60,
-    slidesToScroll: 1,
-    draggable: false,
-    accessibility: false,
-    variableWidth: true,
-    prevArrow: $(".custom-prev"),
-    nextArrow: $(".custom-next"),
-    responsive: [{
-            breakpoint: 2000,
-            settings: {
-                slidesToShow: 5,
+  document.addEventListener('DOMContentLoaded', function () {
+      const swiper = new Swiper('.my-slider-container', {
+        spaceBetween: 20,
+        slidesPerView: "auto",
+        centeredSlides: true,
+        updateOnWindowResize: true,
+        effect: "coverflow",
+        allowTouchMove: false,
 
-             },
-         },
-         {
-             breakpoint: 992,
-             settings: {
-                 slidesToShow: 3,
-                 slidesToScroll: 1,
-                 centerMode: false,
-             },
-         },
-         {
-             breakpoint: 767.9,
-             settings: {
-                 slidesToShow: 3,
-                 slidesToScroll: 1,
-                 centerMode: false,
-                 centerPadding: 30,
-               
-             },
-         },
-         {
-             breakpoint: 425,
-             settings: {
-                 slidesToShow: 1,
-                 slidesToScroll: 2,
-               
-             },
-         },
-     ],
- });
+
+        breakpoints: {
+          320: {
+            loop: true,
+            slidesPerView: 1,
+            effect: "coverflow",
+          },
+
+          768: {
+            loop: false,
+            slidesPerView: "auto",
+            effect: 'coverflow',
+
+            coverflowEffect: {
+              rotate: 0,
+              stretch: 0,
+              depth: 420,
+              modifier: 1,
+              slideShadows: false
+            },
+          },
+          1280: {
+            slidesPerView: "auto",
+            effect: 'coverflow',
+          }
+        },
+        coverflowEffect: {
+          rotate: 0,
+          stretch: 0,
+          depth: 320,
+          modifier: .5,
+          slideShadows: false
+        },
+        navigation: {
+          nextEl: '.swiper-button-prev',
+          prevEl: '.swiper-button-next',
+        },
+        pagination: {
+          el: '.pagination',
+          clickable: true,
+        },
+      });
+
+      swiper.on('slideChangeTransitionStart', function () {
+        swiper.updateSlides();
+        swiper.slideReset();
+      });
+      swiper.slideTo(2);
+    });
